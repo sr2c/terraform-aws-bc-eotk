677a181b8d3598dc262d325a6bd04b15680206a1
Iain Learmonth <irl@sr2.uk>
2022-12-07 14:32:40 +0000

    feat(configure): unpack the zip and configure eotk

--- templates/configure.sh
+++ templates/configure.sh
@@ -3,3 +3,15 @@
 set -e
 
 aws s3 cp s3://${bucket_name}/configuration.zip /root/configuration.zip
+
+cd /root
+unzip configuration.zip
+mv sites.conf /home/ubuntu/eotk/
+chown ubuntu:ubuntu /home/ubuntu/eotk/sites.conf
+mkdir -p /home/ubuntu/eotk/secrets.d
+mv *.key /home/ubuntu/eotk/secrets.d/
+chown ubuntu:ubuntu -R /home/ubuntu/eotk/secrets.d
+cd /home/ubuntu/eotk
+sudo -u ubuntu ./eotk configure sites.conf
+mv /root/*.cert /root/*.pem /home/ubuntu/eotk/projects.d/sites.d/ssl.d/
+sudo -u ubuntu ./eotk bounce sites
