0fd56834415a4106486ca6ca1ff83053dcda1c0c
Silvio Rhatto <rhatto@riseup.net>
2023-04-05 09:52:17 +0000

    fix(eotk): set keys/certs ownership and perms in configure script

--- templates/configure.sh
+++ templates/configure.sh
@@ -11,7 +11,12 @@ chown ubuntu:ubuntu /home/ubuntu/eotk/sites.conf
 mkdir -p /home/ubuntu/eotk/secrets.d
 mv *.key /home/ubuntu/eotk/secrets.d/
 chown ubuntu:ubuntu -R /home/ubuntu/eotk/secrets.d
+chmod 640 /home/ubuntu/eotk/secrets.d/*.v3pub.key
+chmod 600 /home/ubuntu/eotk/secrets.d/*.v3sec.key
 cd /home/ubuntu/eotk
 sudo -u ubuntu ./eotk configure sites.conf
 mv /root/*.cert /root/*.pem /home/ubuntu/eotk/projects.d/sites.d/ssl.d/
+chown ubuntu:ubuntu -R /home/ubuntu/eotk/projects.d/sites.d/ssl.d
+chmod 640 /home/ubuntu/eotk/projects.d/sites.d/ssl.d/*-v3.cert
+chmod 600 /home/ubuntu/eotk/projects.d/sites.d/ssl.d/*-v3.pem
 sudo -u ubuntu ./eotk bounce sites
